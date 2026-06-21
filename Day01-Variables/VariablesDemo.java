2ba9f1bc7ae66eded3a8710d0c9057c963eee0fe
Hari Ganesh <nanthan.antha.paiyan@gmail.com>
2026-06-21 11:54:10 +0530

    Create VariablesDemo.java

---
+++ Day01-Variables/VariablesDemo.java
@@ -0,0 +1,16 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double cgpa = 8.5;
+        char grade = 'A';
+        boolean placed = false;
+        String name = "Hari Ganesh";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Placed: " + placed);
+    }
+}
